a50f38b95fef718c4cb74d037a986375008b1004
Sumit kumar <sumitkumar151294@gmail.com>
2023-09-28 15:13:49 +0530

    Last Update

--- src/Container/Home.js
+++ src/Container/Home.js
@@ -1,50 +1,22 @@
 import React from 'react'
-// import Header from '../Component/Header/Header'
-// import SectionHero from '../Component/SectionHero/SectionHero'
-// import Shop from '../Component/Shop/Shop'
-// import LetShop from '../Component/LetShop/LetShop'
-// import GiftCard from '../Component/GiftCard/GiftCard'
-// import Footer from '../Component/Footer/Footer'
-// import NewHeader from '../Component/NewHeader/NewHeader'
-
-import AboutUs from '../Component/AboutUs/AboutUs'
-import Blog from '../Component/Blog/Blog'
-import ContactUs from '../Component/ContactUs/ContactUs'
-import FAQs from '../Component/FAQs/FAQs'
-import MyProfile from '../Component/MyProfile/MyProfile'
-// import FAQs from '../Component/FAQs/FAQs'
-// import Blog from '../Component/Blog/Blog'
-// import MyProfile from '../Component/MyProfile/MyProfile'
-// import ContactusContent from '../Component/ContactUsContent/ContactusContent'
-// import GrayBox from '../Component/GrayBox/GrayBox'
-// import OrderSummary from '../Component/OrderSummary/OrderSummary'
-// import OrderContainer from '../Component/OrderContainer/OrderContainer'
+import Header from '../Component/Header/Header'
+import NewHeader from '../Component/NewHeader/NewHeader'
+import SectionHero from '../Component/SectionHero/SectionHero'
+import Shop from '../Component/Shop/Shop'
+import LetShop from '../Component/LetShop/LetShop'
+import GiftCard from '../Component/GiftCard/GiftCard'
+import Footer from '../Component/Footer/Footer'
 
 const Home = () => {
     return (
         <>
-            {/* <Header />
+            <Header />
             <NewHeader />
-            <GrayBox />
-            <NewHeader />
-            <OrderSummary />
-            <OrderContainer /> */}
-
-            {/* <SectionHero />
+            <SectionHero />
             <Shop />
             <LetShop />
-            <GiftCard /> */}
-
-            {/* <Footer /> */}
-
-            {/* <ContactUs /> */}
-            {/* <AboutUs /> */}
-            {/* <FAQs /> */}
-            {/* <Blog /> */}
-            <MyProfile />
-
-
-
+            <GiftCard />
+            <Footer />
         </>
     )
 }
