f094fad617a0263cb948839bc8a80b36a5bcff0d
Sumit Kumar <sumitkumar151294@gmail.com>
2023-10-03 18:09:31 +0530

    nisha_AdminUI

--- src/Admin/Sidebar/Sidebar.js
+++ src/Admin/Sidebar/Sidebar.js
@@ -7,100 +7,114 @@ import clists from '../../assets/img/clists.png'
 import blog from '../../assets/img/blog.png'
 import faq from '../../assets/img/faq.png'
 import cnlists from '../../assets/img/cnlist.png'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 
 const Sidebar = () => {
+    const location = useLocation();
     return (
         <div className="deznav">
             <div className="deznav-scroll">
                 <ul className="metismenu" id="menu">
-                    <li><Link to="/LC-admin" className="ai-icon" aria-expanded="false">
-                        <img className="w-20px" src={vendor} />
-                        <span className="nav-text">Dashbaord</span>
-                    </Link>
-
+                    <li className={location.pathname === '/LC-admin' ? 'mm-active' : ''}>
+                        <Link to="/LC-admin" className="ai-icon" aria-expanded="false">
+                            <img className="w-20px" src={vendor} alt="Vendor" />
+                            <span className="nav-text">Dashboard</span>
+                        </Link>
                     </li>
 
-                    <li><Link to="/LC-admin/vendornmaster" className="ai-icon" href="vendormaster.html" aria-expanded="false">
+                    <li className={location.pathname === '/LC-admin/vendornmaster' ? 'mm-active' : ''}>
+                        <Link to="/LC-admin/vendornmaster" className="ai-icon" href="vendormaster.html" aria-expanded="false">
                         <img className="w-20px" src={vendor} />
                         <span className="nav-text">Vendor Master</span>
                     </Link>
 
                     </li>
 
-                    <li><Link to="/LC-admin/allocationmaster" className="ai-icon" aria-expanded="false">
+                    <li className={location.pathname === '/LC-admin/allocationmaster' ? 'mm-active' : ''}>
+                        <Link to="/LC-admin/allocationmaster" className="ai-icon" aria-expanded="false">
                         <img className="w-20px" src={allocation} />
                         <span className="nav-text">Allocated Master</span>
                     </Link>
 
                     </li>
 
-                    <li><Link to='/LC-admin/clientmaster' className="ai-icon" href="clientmaster.html" aria-expanded="false">
+                    <li className={location.pathname === '/LC-admin/clientmaster' ? 'mm-active' : ''}>
+                        <Link to='/LC-admin/clientmaster' className="ai-icon" href="clientmaster.html" aria-expanded="false">
                         <img className="w-20px" src={clientmaster} />
                         <span className="nav-text">Client Master</span>
                     </Link>
 
                     </li>
 
-                    <li><Link to='/LC-admin/rolemaster' className="ai-icon" aria-expanded="false">
+                    <li className={location.pathname === '/LC-admin/rolemaster' ? 'mm-active' : ''}>
+                        <Link to='/LC-admin/rolemaster' className="ai-icon" aria-expanded="false">
                         <img class="w-20px" src={rolemaster} />
                         <span class="nav-text">Role Master</span>
                     </Link>
 
                     </li>
 
-                    <li><Link to='/LC-admin/productcata' className="ai-icon" aria-expanded="false">
+                    <li className={location.pathname === '/LC-admin/productcata' ? 'mm-active' : ''}>
+                        <Link to='/LC-admin/productcata' className="ai-icon" aria-expanded="false">
                         <img className="w-20px" src={clists} />
                         <span className="nav-text">Product Categories</span>
                     </Link>
                     </li>
 
-                    <li><Link to='/LC-admin/productcatalouge' className="ai-icon" aria-expanded="false">
+                    <li className={location.pathname === '/LC-admin/productcatalouge' ? 'mm-active' : ''}>
+                        <Link to='/LC-admin/productcatalouge' className="ai-icon" aria-expanded="false">
                         <img className="w-20px" src={clists} />
                         <span className="nav-text">Product Catalouge</span>
                     </Link>
                     </li>
 
-                    <li><Link to='/LC-admin/orders' className="ai-icon" aria-expanded="false">
+                    <li className={location.pathname === '/LC-admin/orders' ? 'mm-active' : ''}>
+                        <Link to='/LC-admin/orders' className="ai-icon" aria-expanded="false">
                         <img className="w-20px" src={clists} />
                         <span className="nav-text">Orders</span>
                     </Link>
                     </li>
 
-                    <li><Link to='/LC-admin/cms' className="ai-icon" aria-expanded="false">
+                    <li className={location.pathname === '/LC-admin/cms' ? 'mm-active' : ''}>
+                        <Link to='/LC-admin/cms' className="ai-icon" aria-expanded="false">
                         <img className="w-20px" src={clists} />
                         <span className="nav-text">CMS</span>
                     </Link>
                     </li>
 
-                    <li><Link to='/LC-admin/blog' className="ai-icon" aria-expanded="false">
+                    <li className={location.pathname === '/LC-admin/blog' ? 'mm-active' : ''}>
+                        <Link to='/LC-admin/blog' className="ai-icon" aria-expanded="false">
                         <img className="w-20px" src={blog} />
                         <span className="nav-text">Blog Master</span>
                     </Link>
                     </li>
 
 
-                    <li><Link to='/LC-admin/faqs' className="ai-icon" aria-expanded="false">
+                    <li className={location.pathname === '/LC-admin/faqs' ? 'mm-active' : ''}>
+                        <Link to='/LC-admin/faqs' className="ai-icon" aria-expanded="false">
                         <img className="w-20px" src={faq} />
                         <span className="nav-text">FAQ'S Master</span>
                     </Link>
                     </li>
 
 
-                    <li><Link to="/LC-admin/contactlist" className="ai-icon" aria-expanded="false">
+                    <li className={location.pathname === '/LC-admin/contactlist' ? 'mm-active' : ''}>
+                        <Link to="/LC-admin/contactlist" className="ai-icon" aria-expanded="false">
                         <img className="w-20px" src={cnlists} />
                         <span className="nav-text">Contacted List Master</span>
                     </Link>
                     </li>
 
-                    <li><Link to="/LC-admin/customerlist" className="ai-icon" aria-expanded="false">
+                    <li className={location.pathname === '/LC-admin/customerlist' ? 'mm-active' : ''}>
+                        <Link to="/LC-admin/customerlist" className="ai-icon" aria-expanded="false">
                         <img className="w-20px" src={cnlists} />
                         <span className="nav-text">Customer List</span>
                     </Link>
                     </li>
 
-                    <li><Link to="/LC-admin/emailtemplate" className="ai-icon" aria-expanded="false">
+                    <li className={location.pathname === '/LC-admin/emailtemplate' ? 'mm-active' : ''}>
+                        <Link to="/LC-admin/emailtemplate" className="ai-icon" aria-expanded="false">
                         <img className="w-20px" src={cnlists} />
                         <span className="nav-text">Email Templates</span>
                     </Link>
