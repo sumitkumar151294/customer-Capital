8ead5007e67c4b4bc8dbb200aeb867c4cb6bafcc
sumit kumar <sumitkumar151294@gmail.com>
2023-10-18 18:03:34 +0530

    issue fixed

--- src/Container/Home.js
+++ src/Container/Home.js
@@ -6,13 +6,20 @@ import Shop from '../Component/Shop/Shop'
 import LetShop from '../Component/LetShop/LetShop'
 import GiftCard from '../Component/GiftCard/GiftCard'
 import Footer from '../Component/Footer/Footer'
-import Login from '../Component/Login/Login'
+// import Login from '../Component/Login/Login'
 
 const Home = () => {
     return (
         <>
             <Header />
-            <Login />
+            <NewHeader />
+            <SectionHero />
+            <Shop />
+            <LetShop />
+            <GiftCard />
+        <Footer />
+          
+            {/* <Login /> */}
         </>
     )
 }
