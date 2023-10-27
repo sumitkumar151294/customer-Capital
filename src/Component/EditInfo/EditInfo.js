902293475a38f36cc2a3ea8bebc3aa553a7073fe
Sumit kumar <sumitkumar151294@gmail.com>
2023-10-27 16:46:55 +0530

    api validations

--- src/Component/EditInfo/EditInfo.js
+++ src/Component/EditInfo/EditInfo.js
@@ -7,19 +7,56 @@ const EditInfo = () => {
 
     const dispatch = useDispatch();
 
-    const [userData, setUserData] = useState({ fname: '', lname: '', mobile: '', email: '', password: '' })
+    const [userData, setUserData] = useState({ fname: '', lname: '', mobile: '', email: '', password: '', gender: '' })
+    const [errors, setErrors] = useState({ fname: '', lname: '', mobile: '', email: '', password: '', gender: '' });
 
     const handleChange = (e, fieldname) => {
         setUserData({
             ...userData,
             [fieldname]: e.target.value,
         });
+
+        // Remove the error message when the user starts typing
+        setErrors({
+            ...errors,
+            [fieldname]: '',
+        });
+
     }
 
     const handleSubmit = (e) => {
+        
         e.preventDefault()
-        console.log(userData)
-        dispatch(onUpdateSubmit(userData))
+        // console.log(userData)
+        // dispatch(onUpdateSubmit(userData))
+
+        let isValid = true;
+        const newErrors = { ...errors };
+
+        // Check if fields are empty and set corresponding error messages
+        for (const key in userData) {
+            debugger
+            if (userData[key] === '') {
+                newErrors[key] = 'This field is ';
+                isValid = false;
+            } else {
+                newErrors[key] = '';
+            }
+        }
+        // Email validation using the regexEmail pattern
+        const regexEmail = /[a-zA-Z0-9]+([\_\.\-{1}])?[a-zA-Z0-9]+\@[a-zA-Z0-9]+(\.[a-zA-Z\.]+)/g;
+        if (!regexEmail.test(userData.secret)) {
+            newErrors.secret = 'Invalid email format';
+            isValid = false;
+
+        }
+        setErrors(newErrors);
+        debugger
+        if (isValid) {
+            debugger
+            dispatch(onUpdateSubmit(userData))
+        }
+
     }
 
     return (
@@ -38,41 +75,46 @@ const EditInfo = () => {
                                         </div>
                                         <div class="col-md-6 input-main">
                                             <div class="floating-box ">
-                                                <input name="ctl00$txtname" type="text" id="ctl00_txtname" class="floating-input" placeholder="First Name"
+                                                <input name="fname" type="text" id="ctl00_txtname" class="floating-input" placeholder="First Name"
                                                     onChange={(e) => handleChange(e, 'fname')} />
                                                 <span id="ctl00_RequiredFieldValidator3" style={{ display: 'none', color: 'red' }}
 
                                                 >*</span>
+                                                <p className="text-danger">{errors.fname}</p>
                                             </div>
                                         </div>
                                         <div class="col-md-6 input-main">
                                             <div class="floating-box ">
-                                                <input name="ctl00$txtlastname" type="text" id="ctl00_txtlastname" class="floating-input" placeholder="Last Name"
-                                                onChange={(e) => handleChange(e, 'lname')} />
+                                                <input name="lname" type="text" id="ctl00_txtlastname" class="floating-input" placeholder="Last Name"
+                                                    onChange={(e) => handleChange(e, 'lname')} />
                                                 <span id="ctl00_RequiredFieldValidator6" style={{ display: 'none', color: 'red' }}>*</span>
+                                                <p className="text-danger">{errors.lname}</p>
                                             </div>
                                         </div>
                                         <div class="col-md-12 input-main">
                                             <div class="floating-box ">
-                                                <input name="ctl00$txtmobile" type="text" id="ctl00_txtmobile" class="floating-input" placeholder="Enter 10 digit mobile number"
-                                                onChange={(e) => handleChange(e, 'mobile')} />
+                                                <input name="mobile" type="text" id="ctl00_txtmobile" class="floating-input" placeholder="Enter 10 digit mobile number"
+                                                    onChange={(e) => handleChange(e, 'mobile')} />
                                                 <span id="ctl00_RequiredFieldValidator4" style={{ display: 'none', color: 'red' }}>*</span>
+                                                <p className="text-danger">{errors.mobile}</p>
                                             </div>
                                         </div>
                                         <div class="col-md-12 input-main">
                                             <div class="floating-box ">
-                                                <input name="ctl00$txtemail2" type="text" id="ctl00_txtemail2" class="floating-input" placeholder="Enter Email" 
-                                                onChange={(e) => handleChange(e, 'email')}/>
+                                                <input name="email" type="text" id="ctl00_txtemail2" class="floating-input" placeholder="Enter Email"
+                                                    onChange={(e) => handleChange(e, 'email')} />
                                                 <span id="ctl00_RequiredFieldValidator5" style={{ display: 'none', color: 'red' }}>*</span>
+                                                <p className="text-danger">{errors.email}</p>
                                             </div>
                                         </div>
 
                                         <div class="col-md-12 input-main">
                                             <div class="floating-box ">
-                                                <input name="ctl00$txtconfirmpassword" type="password" id="ctl00_txtconfirmpassword" class="floating-input" placeholder="Confirm Password"
-                                                onChange={(e) => handleChange(e, 'password')} />
+                                                <input name="password" type="password" id="ctl00_txtconfirmpassword" class="floating-input" placeholder="Confirm Password"
+                                                    onChange={(e) => handleChange(e, 'password')} />
                                                 <div class="showpassbtn"><i id="showpass2" class="fas fa-eye-slash"></i></div>
                                                 <span id="ctl00_RequiredFieldValidator7" style={{ display: 'none', color: 'red' }}>*</span>
+                                                <p className="text-danger">{errors.password}</p>
 
                                             </div>
                                         </div>
@@ -80,10 +122,12 @@ const EditInfo = () => {
                                             <div class="radiobtnlist signupgender">
                                                 <table id="ctl00_rbtnlist" border="0">
                                                     <tr>
-                                                        <td><input id="ctl00_rbtnlist_0" type="radio" name="ctl00$rbtnlist" value="Male" /><label for="ctl00_rbtnlist_0">Male</label></td><td><input id="ctl00_rbtnlist_1" type="radio" name="ctl00$rbtnlist" value="Female" /><label for="ctl00_rbtnlist_1">Female</label></td>
+                                                        <td><input id="ctl00_rbtnlist_0" type="radio" name="gender" value={userData.gender} onChange={(e) => handleChange(e, "gender")}  /><label for="ctl00_rbtnlist_0">Male</label></td><td><input id="ctl00_rbtnlist_1" type="radio" name="ctl00$rbtnlist" value="Female" /><label for="ctl00_rbtnlist_1">Female</label></td>
                                                     </tr>
                                                 </table>
+
                                             </div>
+                                            
                                         </div>
 
 
